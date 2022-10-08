036fbedd463bdaf9cbcd27496b312fa88a4eb3bd
Yannick Marsot <yannmarsot@hotmail.fr>
2022-10-08 18:28:54 +0200

    ajout style

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,22 +7,33 @@ import colors from '../utils/style/colors'
 const CardLabel = styled.span`
   color: #5843e4;
   font-size: 22px;
-  font-weight: bold;
+  font-weight: normal;
+  padding-left: 15px;
+`
+
+const CardTitle = styled.span`
+  color: black;
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
 `
 
 const CardImage = styled.img`
-  height: 80px;
-  width: 80px;
+  height: 150px;
+  width: 150px;
+  align-self: center;
   border-radius: 50%;
 `
 
 const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   padding: 15px;
   background-color: ${colors.backgroundLight};
   border-radius: 30px;
-  width: 350px;
+  width: 300px;
+  height: 300px;
   transition: 200ms;
   &:hover {
     cursor: pointer;
@@ -35,7 +46,7 @@ function Card({ label, title, picture }) {
     <CardWrapper>
       <CardLabel>{label}</CardLabel>
       <CardImage src={picture} alt="freelance" />
-      <span>{title}</span>
+      <CardTitle>{title}</CardTitle>
     </CardWrapper>
   )
 }
@@ -47,8 +58,8 @@ Card.propTypes = {
 }
 
 Card.defaultProps = {
-  title: 'Mon titre par défaut',
-  label: 'Mon label par défaut',
+  title: '',
+  label: '',
   picture: DefaultPicture,
 }
 
