b3a6c364bec8bcd0644b8c30d29b7eedc29b0642
Yannick Marsot <yannmarsot@hotmail.fr>
2022-10-09 14:32:53 +0200

    fin exercice

--- src/index.jsx
+++ src/index.jsx
@@ -7,26 +7,27 @@ import Header from './components/Header'
 import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
-import { createGlobalStyle } from 'styled-components'
+import Footer from './components/Footer'
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider, SurveyProvider } from './utils/context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`
-
 root.render(
   <BrowserRouter>
-    <GlobalStyle />
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/survey/:questionNumber" element={<Survey />}></Route>
-      <Route path="/results" element={<Results />}></Route>
-      <Route path="/freelances" element={<Freelances />}></Route>
-      <Route path="*" element={<Error />}></Route>
-    </Routes>
+    <ThemeProvider>
+      <SurveyProvider>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/survey/:questionNumber" element={<Survey />}></Route>
+          <Route path="/results" element={<Results />}></Route>
+          <Route path="/freelances" element={<Freelances />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+        <Footer />
+      </SurveyProvider>
+    </ThemeProvider>
   </BrowserRouter>
 )
